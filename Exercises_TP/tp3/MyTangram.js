71f99ad2f919b6e4e3a35a00ef5f1cf423289403
David <up201705373@fe.up.pt>
2019-03-06 16:31:55 +0000

    Added files for tp3

---
+++ Exercises_TP/tp3/MyTangram.js
@@ -0,0 +1,113 @@
+/**
+ * MyTangram
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTangram extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.diamond = new MyDiamond(this.scene);
+        this.triangle = new MyTriangle(this.scene);
+        this.parallelogram = new MyParallelogram(this.scene);
+        this.triangleBig1 = new MyTriangleBig(this.scene);
+        this.triangleSmall1 = new MyTriangleSmall(this.scene);
+        this.triangleBig2 = new MyTriangleBig(this.scene);
+		this.triangleSmall2 = new MyTriangleSmall(this.scene);
+	}
+	display() {
+		this.scene.pushMatrix();
+
+        var diamondR = [Math.cos(Math.PI / 4.0), Math.sin(Math.PI / 4.0), 0.0, 0.0,
+                        -Math.sin(Math.PI / 4.0), Math.cos(Math.PI / 4.0), 0.0, 0.0,
+                        0.0, 0.0, 1.0, 0.0,
+                        0.0, 0.0, 0.0, 1.0];
+
+        var diamondR2 = [Math.cos(-Math.PI / 6.0), Math.sin(-Math.PI / 6.0), 0.0, 0.0,
+                        -Math.sin(-Math.PI / 6.0), Math.cos(-Math.PI / 6.0), 0.0, 0.0,
+                        0.0, 0.0, 1.0, 0.0,
+                        0.0, 0.0, 0.0, 1.0];
+                    
+        var diamondT = [1.0, 0.0, 0.0, 0.0,
+                        0.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 0.0,
+                        -Math.sqrt(2) / 2.0, Math.sqrt(2) / 2.0, 0.0, 1.0];
+        
+        this.scene.multMatrix(diamondR2);
+        this.scene.multMatrix(diamondT);
+        this.scene.multMatrix(diamondR);
+
+        this.diamond.display();
+
+        // ---- 
+
+        this.scene.popMatrix();
+        this.scene.pushMatrix();
+        
+        this.scene.rotate(-Math.PI / 4 - Math.PI / 6, 0.0, 0.0, 1.0);
+        this.scene.translate(0, -1, 0);
+        
+        this.triangleSmall1.display();
+
+        // ----
+
+        this.scene.popMatrix();
+        this.scene.pushMatrix();
+        
+        this.scene.rotate(- Math.PI / 6, 0.0, 0.0, 1.0);
+        this.scene.translate(-Math.sqrt(2.0), 0.0, 0);
+        this.scene.rotate(-Math.PI / 4, 0.0, 0.0, 1.0);
+        this.scene.scale(1.0, -1.0, 1.0);
+
+        this.parallelogram.display();  
+
+        // ----
+
+        this.scene.popMatrix();
+        this.scene.pushMatrix();
+        
+        this.scene.rotate(- Math.PI / 6, 0.0, 0.0, 1.0);
+        this.scene.translate(0, Math.sqrt(2), 0);
+        this.scene.rotate(3 * Math.PI / 4, 0.0, 0.0, 1.0);
+        this.scene.translate(-2, 0, 0);
+
+       this.triangleBig1.display();
+
+        // ----
+
+        this.scene.popMatrix();
+        this.scene.pushMatrix();
+        
+        this.scene.rotate(- Math.PI / 6, 0.0, 0.0, 1.0);
+        this.scene.translate(Math.sqrt(8), 2 - Math.sqrt(2), 0);
+        this.scene.rotate(Math.PI, 0.0, 0.0, 1.0);
+
+        this.triangleBig2.display();
+    
+        // ----
+
+        this.scene.popMatrix();
+        this.scene.pushMatrix();
+        
+        this.scene.rotate(- Math.PI / 6, 0.0, 0.0, 1.0);
+        this.scene.translate(- Math.sqrt(2), Math.sqrt(2), 0);
+        this.scene.rotate(- 3 * Math.PI / 4, 0, 0, 1);
+
+        this.triangle.display();
+
+        // ----
+
+        this.scene.popMatrix();
+        this.scene.pushMatrix();
+
+        this.scene.rotate(- Math.PI / 6, 0.0, 0.0, 1.0);
+        this.scene.translate(2 + Math.sqrt(8), 2 - Math.sqrt(2), 0);
+        this.scene.rotate(- 3 * Math.PI / 4, 0, 0, 1);
+        this.scene.translate(0, -1, 0);
+        
+        this.triangleSmall2.display();
+        this.scene.popMatrix();
+	}
+}
---
+++ Exercises_TP/tp3/MyTriangleBig.js
@@ -0,0 +1,32 @@
+/**
+ * MyTriangleBig
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTriangleBig extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			2, 0, 0,	//0
+			0, 2, 0,	//1
+			-2, 0, 0,	//2
+		];
+
+		this.normals = [
+
+			0,0,1,
+			0,0,1,
+			0,0,1,
+		]
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			0, 1, 2,
+		];
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	}
+}
