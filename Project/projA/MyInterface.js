6d47be3a0b2ce7c7795d926d057d50c03b8d8be6
David <up201705373@fe.up.pt>
2019-03-20 11:48:20 +0000

    Added initial files for project

---
+++ Project/projA/MyInterface.js
@@ -0,0 +1,21 @@
+/**
+* MyInterface
+* @constructor
+*/
+class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        // call CGFinterface init
+        super.init(application);
+        // init GUI. For more information on the methods, check:
+        // http://workshop.chromeexperiments.com/examples/gui
+        this.gui = new dat.GUI();
+        
+        var obj = this;
+
+        return true;
+    }
+}
